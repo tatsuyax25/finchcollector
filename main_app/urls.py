5c1ddd88f0f8166c04c1ccaaae0c06872e3dcd82
Miguel Angel Urena <urena_miguel82@outlook.com>
2021-10-21 11:50:50 -0700

    Update

--- main_app/urls.py
+++ main_app/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
     path('finches/<int:finch_id>/', views.finches_detail, name='detail'),
     # new route used to show a form and create a cat
     path('finches/create/', views.FinchCreate.as_view(), name='finches_create'),
+    # Add the new routes below
+    path('finches/<int:pk>/update/', views.FinchUpdate.as_view(), name='finches_update'),
+    path('finches/<int:pk>/delete/', views.FinchDelete.as_view(), name='finches_delete'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 # render in django renders template
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Finch
 
 # Add the following import
@@ -21,12 +21,21 @@ def finches_index(request):
     finches = Finch.objects.all()
     return render(request, 'finches/index.html', { 'finches': finches })
 
+def finches_detail(request, finch_id):
+    finch = Finch.objects.get(id=finch_id)
+    return render(request, 'finches/detail.html', {'finch': finch})
+
 class FinchCreate(CreateView):
     model = Finch
     fields = '__all__'
     success_url ='/finches/'
 
-def finches_detail(request, finch_id):
-    finch = Finch.objects.get(id=finch_id)
-    return render(request, 'finches/detail.html', {'finch': finch})
+class FinchUpdate(UpdateView):
+    model = Finch
+    # Let's disallow the renaming of a cat by excluding the name field!
+    fields = ['breed', 'description', 'age']
+
+class FinchDelete(DeleteView):
+    model = Finch
+    success_url = '/finches/'
     
